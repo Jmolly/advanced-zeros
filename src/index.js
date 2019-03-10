a38b73729382cb8559f3a3748df4a0c39b34c6f9
Jmolly <roshkovskypetr@gmail.com>
2019-03-11 01:08:46 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  
+  let minZerosCount = Number.MAX_VALUE;
+
+  for (let i = 2; i <= base; i++) {
+    if (isPrime(i, base)) {
+      let primesInExp = 0;
+      let zerosCount = 0;
+      let expForZeros = 1;
+      let exponent = 0;
+
+      while (base % i == 0) {
+        base /= i;
+        exponent++;
+      }
+
+      while (primesInExp < number) {
+        primesInExp = Math.pow(i, expForZeros++);
+        zerosCount += Math.floor(number / primesInExp);
+      }
+
+      minZerosCount = Math.min(minZerosCount, Math.floor(zerosCount / exponent));
+    }
+  }
+
+  return minZerosCount;
+};
+
+function isPrime(num, base) {
+  for (let i = 2; i < num; i++) {
+    if (num % i == 0) return false;
+  }
+
+  return base % num == 0;
 }
